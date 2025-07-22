f6535aca813f303beb46cd3b552ced0003c23f73
Naif <nalmohammed@nic.gov.sa>
2025-07-22 16:14:37 +0300

    new project

--- manifiests/kafka-chart/run.sh
+++
@@ -1,5 +0,0 @@
-helm repo add strimzi https://strimzi.io/charts/
-helm repo update
-
-helm install strimzi-cluster-operator2 strimzi/strimzi-kafka-operator \
-  --namespace kafka --create-namespace 
--- manifiests/monitoring/run.sh
+++
@@ -1,5 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-
-helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
-  --namespace monitoring -f values.yaml
